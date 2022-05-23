7824dc331c1335da32d68b168314ab70b3088f1e
Letícia Marques <leticia.marquess@protonmail.com>
2022-05-23 20:02:06 -0300

    Added mutex

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,7 +6,7 @@
 /*   By: lemarque <lemarque@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 00:47:29 by lemarque          #+#    #+#             */
-/*   Updated: 2022/05/23 17:16:21 by lemarque         ###   ########.fr       */
+/*   Updated: 2022/05/23 20:01:34 by lemarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,15 @@ long get_time(void)
 static void	*call_actions(void *philo_void)
 {
 	t_philo *philo;
+	pthread_mutex_t	get_timestamp;
 
+	pthread_mutex_init(&get_timestamp, NULL);
 	philo = (t_philo *)philo_void;
 	if (philo->id % 2 == 0)
 		usleep(50);
-	pthread_mutex_lock(philo->lock_meals);
+	pthread_mutex_lock(&get_timestamp);
 	philo->data->timestamp = get_time();
-	pthread_mutex_unlock(philo->lock_meals);
+	pthread_mutex_unlock(&get_timestamp);
 	while (philo->data->dinner_is_over == 0)
 	{
 		eat(philo);
@@ -45,22 +47,21 @@ static void	*monitor_philos(void *philos_void)
 {
 	t_philo	*philos;
 	int		i;
-	int		philos_num;
+	pthread_mutex_t	finish_dinner;
 
-
+	pthread_mutex_init(&finish_dinner, NULL);
 	philos = (t_philo *)philos_void;
-	philos_num = philos[0].data->philos_number;
 	while (finished_dinner(philos) == 1)
 	{
 		i = -1;
-		while (++i < philos_num)
+		while (++i < philos[0].data->philos_number)
 		{
 			if ( (get_time() - philos[i].last_meal)\
 			 > philos[i].data->time_to_die)
 			{
-				pthread_mutex_lock(philos[i].lock_meals);
+				pthread_mutex_lock(&finish_dinner);
 				philos[i].data->dinner_is_over = 1;
-				pthread_mutex_unlock(philos[i].lock_meals);
+				pthread_mutex_unlock(&finish_dinner);
 				print_actions(DIED, &philos[i]);
 				return (NULL);
 			}
